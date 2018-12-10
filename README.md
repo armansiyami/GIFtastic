995007dac406f02bcf9e5062aa97c764d69c01ea
Arman Siyami <armansiyami@Armans-MBP-16.frontierlocal.net>
2018-12-09 19:54:37 -0800

    master push

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GIFtastic
+
+------------------------------------------------------------------------------------------
+       
+This program allows you to search the GIPHY GIF library by keyword, and then displays the GIF's on the webpage
---
+++ assets/javascript/logic.js
@@ -0,0 +1,66 @@
+
+$( document ).ready(function() { 
+
+		$("#add-movie").on("click", function(event) {
+        event.preventDefault();
+
+        var movie = $("#movie-input").val().trim();
+ 
+        movies.push(movie);
+		console.log(movies);
+        renderButtons();
+      });
+	
+	var movies = ["The Terminator", "Star Wars", "Lord of the Rings"];
+	function renderButtons() {
+        $("#movies-view").empty();
+
+        for (var i = 0; i < movies.length; i++) {
+
+          var a = $("<button class='btn btn-primary'>");
+          a.addClass("movie");
+		  a.attr('id','buttonPress');
+          a.attr("data-name", movies[i]);
+          a.text(movies[i]);
+          $("#movies-view").append(a);
+			
+		  $(".movie").on("click", function() {
+			  $("#gifs-here").empty();
+			  var person = $(this).attr("data-name");
+
+			  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+				person + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+			  $.ajax({
+				url: queryURL,
+				method: "GET"
+			  })
+				.then(function(response) {
+				  console.log(response.data);
+				  var results = response.data;
+				  for (var i = 0; i < results.length; i++) {
+					if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+		
+					  var gifDiv = $("<div>");
+					  var rating = results[i].rating;
+					  var p = $("<p>").text("Rating: " + rating);
+
+					  var personImage = $("<img>");
+					  personImage.attr("src", results[i].images.fixed_height.url);
+
+					  gifDiv.append(p);
+					  gifDiv.append(personImage);
+
+					  $("#gifs-here").prepend(gifDiv);
+					  $("#gifs-here1").prepend(gifDiv);
+	
+					}
+				  }
+				});
+			});
+        }
+      }
+
+      renderButtons();
+
+});
